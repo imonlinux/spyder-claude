8146eb3f730c94421b5ec02d1eed78eb7548828d
imonlinux <39863321+imonlinux@users.noreply.github.com>
2026-04-22 09:16:02 -0700

    Update __init__.py
    Fix CLI approval prompt

--- spyder_claude/widget/__init__.py
+++ spyder_claude/widget/__init__.py
@@ -2,7 +2,14 @@
 # Copyright © spyder-claude contributors
 # Licensed under the terms of the MIT License
 
+from .approval_dialog import ApprovalDialog
+from .approval_server import ApprovalServer
 from .main_widget import ClaudeMainWidget
 from .preferences import ClaudeConfigPage
 
-__all__ = ["ClaudeMainWidget", "ClaudeConfigPage"]
+__all__ = [
+    "ApprovalDialog",
+    "ApprovalServer",
+    "ClaudeConfigPage",
+    "ClaudeMainWidget",
+]
